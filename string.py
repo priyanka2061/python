6ded37391ae0d176af8818d926b88f701ea37b76
priyanka2061 <kumaripriyanka76679707@gmail.com>
2021-12-17 12:29:56 +0530

    this first

---
+++ string.py
@@ -0,0 +1,24 @@
+# how to print string and access and delete all string
+'''str=" prinyanka"
+print(str[-1])
+print (str[-2])
+print(str[2])
+print(str) 
+print("delete all string")
+del(str)'''
+# use of silicing
+str1 =" helloworld"
+print(str1[3:7])
+print(str1[:])
+# use of escape squence
+print("priyanka\n kumari")
+print("priyanka\t kumari")
+print("hello\"wolrd\"")
+print(" priyanka ' kumari'")
+#use of string formatting
+# defualt formatting
+str2=" {} {}".format('priyanka','kumari')
+print(str2)
+# positional formatting
+str3=" {2} {0} {1}".format( ' hy','priyanka','kumari' ,)
+print(str3)
